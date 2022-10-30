0e91f0963220d6116020e060261cc24e1981c95d
Sérgio Vieira <sergiovieira.webb@gmail.com>
2022-10-30 19:55:34 -0300

    fix: adiciona campo password na seed student

--- src/database/seeders/20220922171740-students.js
+++ src/database/seeders/20220922171740-students.js
@@ -3,6 +3,7 @@ module.exports = {
     await queryInterface.bulkInsert('Students', [{
       name: 'Pessoa Estudante 1',
       email: 'estudate1@teste.com',
+      password: '1234567',
       birth_date: '1984-03-05T02:00:00',
       active: true,
       id_course: 2,
@@ -11,12 +12,14 @@ module.exports = {
       name: 'Pessoa Estudante 2',
       email: 'estudante2@teste.com',
       birth_date: '1974-03-05T02:00:00',
+      password: '1234567',
       active: true,
       id_course: 2,
     },
     {
       name: 'Pessoa Estudante 3',
       email: 'estudante3@teste.com',
+      password: '1234567',
       birth_date: '1989-03-05T02:00:00',
       active: false,
       id_course: 3,
